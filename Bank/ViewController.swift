2e1160d5239a4081012914496e2af8d4fa23de46
SEI4X <mashkov0599@gmail.com>
2020-03-20 22:23:13 +0300

    Initial Commit

---
+++ Bank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bank
+//
+//  Created by Alexei Mashkov on 20.03.2020.
+//  Copyright © 2020 Alexei Mashkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
